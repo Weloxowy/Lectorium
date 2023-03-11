216ed32e7c88f3b3d89004ee0e0982b142d59178
Student092767 <s092767@student.kielce.pl>
2023-03-11 15:20:07 +0100

    pliki

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent loginParent = FXMLLoader.load(getClass().getResource("/FXML/b.fxml"));
-        Font.loadFont(getClass().getResourceAsStream("/resources/FONTY/SourceSerifPro-Regular.ttf"), 14);
+        Parent loginParent = FXMLLoader.load(getClass().getResource("/fxml/test.fxml"));
         Scene loginScene = new Scene(loginParent);
-        stage.setTitle("Lectorium");
+        stage.setTitle("GBS");
         stage.setScene(loginScene);
         stage.show();
     }
