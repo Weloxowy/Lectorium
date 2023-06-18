ede964f296018a8bcafcdd106a60693fc9b1394c
Weloxowy <106489185+weloxowy@users.noreply.github.com>
2023-06-18 23:21:42 +0200

    v0.9.9 dokumentacja

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.app"},{"l":"org.example.app.admin"},{"l":"org.example.app.home"},{"l":"org.example.auth"},{"l":"org.example.db"},{"l":"tests"}];updateSearchResults();
